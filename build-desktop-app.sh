0c09e938d4db41fb8457142d703fac3875a71807
Pando85 <pando855@gmail.com>
2017-01-08 23:45:33 +0100

    Improved build app

--- build-desktop-app.sh
+++ build-desktop-app.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-nativefier --name "sharemusic" "https://music.grigri.cloud" --icon ./web/sharemusic/apps/client/static/client/img/favicon32.png
+sudo cp ./web/static/client/img/favicon.ico /usr/share/icons/hicolor/32x32/apps/sharemusic.png
+nativefier --name "sharemusic" "https://music.grigri.cloud" --icon /usr/share/icons/hicolor/32x32/apps/sharemusic.png
+sudo mv sharemusic-linux-x64 /opt/sharemusic
+# Bug starting without permissions
+sudo chown $USER:$GROUP -R /opt/sharemusic
